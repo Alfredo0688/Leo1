fe89561b7ad9fff27a29da1cf77b1495ca36442f
alfredo0688 <alfredonunez0688@gmail.com>
2026-01-31 14:47:05 -0300

    cambios realizados

--- src/clases/Asignatura.java
+++ src/clases/Asignatura.java
@@ -1,7 +1,17 @@
 
 package clases;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "asignaturas")
 public class Asignatura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codigo;
     private String nombre;
     private String descripcion;
--- src/clases/Docente.java
+++ src/clases/Docente.java
@@ -3,8 +3,17 @@ package clases;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table (name = "docentes")
 public class Docente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String legajo;
     private String nombre;
