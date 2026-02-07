bbcce54cf9ea826b087eaf7ea45641a8649f2c90
alfredo0688 <alfredonunez0688@gmail.com>
2026-02-07 11:55:19 -0300

    corrigiendo mambos

--- src/clases/Asignatura.java
+++ src/clases/Asignatura.java
@@ -5,13 +5,12 @@ public class Asignatura {
     private Integer codigo;
     private String nombre;
     private String descripcion;
-
+    private Instituto instituto;
+    
     public Asignatura() {
         this.nombre = "";
         this.descripcion = "";
     }
-    
-    
     
     public Asignatura(String nombre, String descripcion) {
         this.nombre = nombre;
@@ -37,6 +36,15 @@ public class Asignatura {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+    
+    public Instituto getInstituto() {
+        return instituto;
+    }
+
+    public void setInstituto(Instituto instituto) {
+        this.instituto = instituto;
+    }
+
 
     @Override
     public String toString() {
